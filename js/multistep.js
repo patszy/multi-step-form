703e232cf88140f1f6f9c6cf6016aa0cbc67101d
patszy <patszy97@interia.pl>
2024-09-03 15:51:16 +0200

    Form switching

--- js/multistep.js
+++ js/multistep.js
@@ -1,1 +1,19 @@
 console.log("Hello World!");
+
+let cards = document.getElementsByClassName(`card`);
+let nextBtn = document.getElementById(`next`);
+let prevBtn = document.getElementById(`prev`);
+let currentCard = [...cards].filter(card => card.classList.contains(`card--active`))[0];
+let currentStep = [...cards].indexOf(currentCard);
+
+nextBtn.addEventListener(`click`, () => {
+  currentCard = [...cards].filter(card => card.classList.contains(`card--active`))[0];
+  currentCard.classList.remove(`card--active`);
+  cards[++currentStep].classList.add(`card--active`);
+});
+
+prevBtn.addEventListener(`click`, () => {
+  currentCard = [...cards].filter(card => card.classList.contains(`card--active`))[0];
+  currentCard.classList.remove(`card--active`);
+  cards[--currentStep].classList.add(`card--active`);
+});
