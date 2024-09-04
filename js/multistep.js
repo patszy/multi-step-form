ae0e1604cbadc017071a6be43237312c0825d88c
patszy <patszy97@interia.pl>
2024-09-04 12:57:18 +0200

    Commented and fixed bugs

--- js/multistep.js
+++ js/multistep.js
@@ -1,27 +1,26 @@
-console.log("Hello World!");
-
 const multistepForm = document.getElementsByClassName(`multistep__form`)[0];
 const formCards = [...multistepForm.getElementsByClassName(`card`)];
-let currentStep = formCards.findIndex(step => step.classList.contains(`card--active`));
+let currentCard = formCards.findIndex(step => step.classList.contains(`card--active`));
 
-if(currentStep < 0) formCards[currentStep = 0].classList.add(`card--active`);
+//If no active card
+if(currentCard < 0) formCards[currentCard = 0]?.classList.add(`card--active`);
 
 const showCurrentCard = () => {
-  formCards.forEach((card, index) => card.classList.toggle(`card--active`, index === currentStep));
+  formCards.forEach((card, index) => card.classList.toggle(`card--active`, index === currentCard));
 }
 
 multistepForm.addEventListener("click", (e) => {
   e.preventDefault();
-  let inc;
-  if(e.target.matches(`#next`)) inc = 1;
-  else if(e.target.matches(`#prev`)) inc = -1;
-  else return;
-
-  const inputs = [...formCards[currentStep].querySelectorAll(`input`)];
-  const allValid = inputs.every(input => input.reportValidity());
-
-  if(allValid) {
-    currentStep += inc;
-    showCurrentCard()
+  e.stopPropagation();
+  //Listen event only if press button
+  if(!e.target.matches(`#next, #prev`)) return;
+  //Switch left or right
+  const increment = e.target.id === `next` ? 1 : -1;
+  
+  const inputs = [...formCards[currentCard]?.querySelectorAll(`input`)];
+  if(inputs.every(input => input.reportValidity())) {
+    //Limit of switch range
+    currentCard = Math.min(Math.max(currentCard + increment, 0), formCards.length - 1);
+    showCurrentCard();
   };
 });
